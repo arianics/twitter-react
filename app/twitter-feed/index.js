8f138247f6a77cb3328c9b9896998da759595435
Arian Khosravi <arian.khosravi@aofl.com>
2016-01-14 12:22:15 -0800

    filter observable into Italian and english and bind to view

--- app/twitter-feed/index.js
+++ app/twitter-feed/index.js
@@ -5,21 +5,42 @@
   .config(['$routeProvider', function ($routeProvider) {
       $routeProvider.when('/', {
         templateUrl: 'twitter-feed/index.html',
-        controller: 'TwitterFeedController'
+        controller: 'TwitterFeedController',
+        css: 'twitter-feed/index.css'
       });
   }])
-    .controller('TwitterFeedController', ['rx', function (rx) {
+    .controller('TwitterFeedController', ['$scope', 'rx',
+      function ($scope, rx) {
+        $scope.enTweets = [];
+        $scope.itTweets = [];
 
-      var socket = io.connect();
-      var tweetObservable = rx.Observable.fromEventPattern(function add(h) {
-        socket.on('tweet', h);
-      });
-
-      var subscription = tweetObservable.subscribe(
-        function (tweet) {
-          if(tweet.lang === 'en') {
-            console.log(tweet);
-          }
+        var socket = io.connect();
+        var tweetObservable = rx.Observable.fromEventPattern(function add(h) {
+          socket.on('tweet', h);
         });
-    }]);
+
+        var enSubscription = tweetObservable
+          .filter(x => x.lang === 'en')
+          .filter(x => $scope.enTweets.indexOf(x.text) === -1)
+          .map(function (response) {
+              return response.text;
+          })
+          .subscribe(function (tweet) {
+            $scope.$apply(function() {
+              $scope.enTweets.unshift(tweet);
+            });
+          });
+
+        var itSubscription = tweetObservable
+          .filter(x => x.lang === 'it')
+          .filter(x => $scope.itTweets.indexOf(x.text) === -1)
+          .map(function (response) {
+              return response.text;
+          })
+          .subscribe(function (tweet) {
+            $scope.$apply(function() {
+              $scope.itTweets.unshift(tweet);
+            });
+          });
+      }]);
 }());
